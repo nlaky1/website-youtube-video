b00a9be6b03af561ea2a5e03dd2f421cac4771d2
nlaky1 <49218507+nlaky1@users.noreply.github.com>
2024-07-09 23:05:28 +0530

    css update contact button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -87,15 +87,11 @@ const Navbar = ({
             />
           )}
         </div>
-        <div className="hidden md:flex">
+        <div className="hidden md:flex ">
           <Link
             href="/contact"
             className="
-            inline-flex h-12 animate-shimmer items-center justify-center 
-            rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] 
-            bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors
-             focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2
-              focus:ring-offset-slate-50
+           inline-flex h-12 animate-shimmer items-center justify-center rounded-2xl border   bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%]  px-6 font-medium text-slate-200 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50
             "
           >
             Contact
